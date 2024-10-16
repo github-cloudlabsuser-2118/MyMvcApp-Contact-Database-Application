a96f28c6293758e267cc36e7f5ab545f9f1987da
Martin <odl_user_1486694@azurehol1137.onmicrosoft.com>
2024-10-16 10:03:31 +0000

    Fixing UserController and User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,6 @@
-
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +9,12 @@ namespace MyMvcApp.Models
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
 
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
     }
 }
